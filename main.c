a6d8c8a630391c4d5a2367cdb2ae05c42d4053ce
Philiks <fjanopol@gmail.com>
2021-07-11 13:29:50 +0800

    Integrated vm functions

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char* argv[]) {
+    initVM();
+
     Chunk chunk;
     initChunk(&chunk);
 
@@ -16,6 +19,8 @@ int main(int argc, const char* argv[]) {
     writeChunk(&chunk, OP_RETURN, 6);
 
     disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
