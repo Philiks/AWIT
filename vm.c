d3ad2c2664f6adeda5684d013d7d55c86110053e
Philiks <fjanopol@gmail.com>
2021-07-21 14:59:56 +0800

    Changed the chunk to source file.

--- vm.c
+++ vm.c
@@ -1,8 +1,9 @@
 #include "stdio.h"
 
-#include "vm.h"
-#include "debug.h"
 #include "common.h"
+#include "vm.h"
+#include "compiler.h"
+#include "debug.h"
 
 VM vm;
 
@@ -77,8 +78,7 @@ static InterpretResult run() {
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk* chunk) {
-    vm.chunk = chunk;
-    vm.ip = vm.chunk->code;
-    return run();
+InterpretResult interpret(const char* source) {
+    compile(source);
+    return INTERPRET_OK;
 }
