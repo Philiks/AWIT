9aaea7302b9fbe6595cf9536e770e576dfd1259d
Philiks <fjanopol@gmail.com>
2021-07-24 04:28:26 +0800

    Changed printValue() & added valuesEqual()

--- value.c
+++ value.c
@@ -26,5 +26,21 @@ void freeValueArray(ValueArray* array) {
 }
 
 void printValue(Value value) {
-    printf("%g", value);
+    switch (value.type) {
+        case VAL_BOOL:
+            printf(AS_BOOL(value) ? "tama" : "mali");
+            break;
+        case VAL_NULL: printf("null"); break;
+        case VAL_NUMBER: printf("%g", AS_NUMBER(value)); break;
+    }
+}
+
+bool valuesEqual(Value a, Value b) {
+    if (a.type != b.type) return false;
+    switch (a.type) {
+        case VAL_BOOL:      return AS_BOOL(a) == AS_BOOL(b);
+        case VAL_NULL:      return true;
+        case VAL_NUMBER:    return AS_NUMBER(a) == AS_NUMBER(b);
+        default:            return false; // Unreachable.
+    }
 }
