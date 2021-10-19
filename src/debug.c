0bd4a5ce7deaedaf1a8073b1c1598f5b2cb02312
Philiks <fjanopol@gmail.com>
2021-10-19 22:12:37 +0800

    Print chunk's name during disassembling.

--- src/debug.c
+++ src/debug.c
@@ -5,6 +5,8 @@
 #include "value.h"
 
 void disassembleChunk(Chunk* chunk, const char* name) {
+    printf("== %s ==\n", name);
+
     for (int offset = 0; offset < chunk->count;) {
         offset = disassembleInstruction(chunk, offset);
     }
